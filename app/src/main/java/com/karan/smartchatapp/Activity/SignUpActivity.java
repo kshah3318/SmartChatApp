279a9da8c97b92cc621f198ae3033684d881a5d8
kshah3318 <kshah3318@gmail.com>
2017-07-16 15:36:21 +0530

    Added Email
    and registering with email

--- app/src/main/java/com/karan/smartchatapp/Activity/SignUpActivity.java
+++ app/src/main/java/com/karan/smartchatapp/Activity/SignUpActivity.java
@@ -22,6 +22,8 @@ public class SignUpActivity extends AppCompatActivity {
     @BindView(R.id.btnCancel) Button btnCancel;
     @BindView(R.id.signup_etUsernName) EditText etUserName;
     @BindView(R.id.signup_etUserPassword) EditText etPassword;
+    @BindView(R.id.signup_etEmail) EditText etEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +33,7 @@ public class SignUpActivity extends AppCompatActivity {
         btnCancel = (Button) findViewById(R.id.btnCancel);
         etUserName = (EditText) findViewById(R.id.signup_etUsernName);
         etPassword = (EditText) findViewById(R.id.signup_etUserPassword);
+        etEmail=(EditText)findViewById(R.id.signup_etEmail);
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,19 +45,18 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View v) {
               String username=etUserName.getText().toString();
               String password=etPassword.getText().toString();
-                Toast.makeText(getApplicationContext(),username, Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),password, Toast.LENGTH_SHORT).show();
                 QBUser qbUser=new QBUser(username,password);
+                qbUser.setEmail(etEmail.getText().toString());
                 QBUsers.signUp(qbUser).performAsync(new QBEntityCallback<QBUser>() {
                     @Override
                     public void onSuccess(QBUser qbUser, Bundle bundle) {
-
                         Toast.makeText(getApplicationContext(),"Registered Successfully",Toast.LENGTH_LONG);
                         finish();
                     }
                     @Override
                     public void onError(QBResponseException e) {
-
+                        Log.d("ERROR",e.getMessage());
+                       Toast.makeText(getApplicationContext(),e.getMessage().toString(),Toast.LENGTH_LONG).show();
                     }
                 });
             }
@@ -65,7 +67,6 @@ public class SignUpActivity extends AppCompatActivity {
         QBAuth.createSession().performAsync(new QBEntityCallback<QBSession>() {
             @Override
             public void onSuccess(QBSession qbSession, Bundle bundle) {
-
             }
 
             @Override
